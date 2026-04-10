353d1952326613e647e79660353c21bfcc70d934
MSatyam-Mishra <satyam12295@gmail.com>
2026-04-10 15:22:50 +0530

    fixed the text overflow in the segmented button group

---
+++ src/generate_reports_11076.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print('Processing generate_reports_11076.py system updates...')
---
+++ src/generate_reports_11098.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+print('Processing generate_reports_11098.py system updates...')
