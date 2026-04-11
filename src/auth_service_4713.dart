e14d36eddf69216bac24ef2b669e4483e88b374b
MSatyam-Mishra <satyam12295@gmail.com>
2026-04-11 23:57:11 +0530

    optimized the performance of the sortable data table

---
+++ src/auth_service_4713.dart
@@ -0,0 +1,2 @@
+// Auto-generated logic for auth_service_4713.dart
+void main() { print('Executing auth_service_4713.dart tasks...'); }
---
+++ src/state_notifier_1053.dart
@@ -0,0 +1,2 @@
+// Auto-generated logic for state_notifier_1053.dart
+void main() { print('Executing state_notifier_1053.dart tasks...'); }
